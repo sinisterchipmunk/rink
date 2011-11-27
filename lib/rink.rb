c7aeebaa98f83f7e0b95e836c59fcef535246cbc
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-11-27 13:19:21 -0500

    Use autoload to reduce bloat

--- lib/rink.rb
+++ lib/rink.rb
@@ -1,15 +1,11 @@
-#require 'sc-ansi'
-
 require File.expand_path(File.join(File.dirname(__FILE__), "core_ext/object"))
-require File.expand_path(File.join(File.dirname(__FILE__), "rink/delegation"))
-require File.expand_path(File.join(File.dirname(__FILE__), 'rink/namespace'))
-require File.expand_path(File.join(File.dirname(__FILE__), "rink/lexer"))
-require File.expand_path(File.join(File.dirname(__FILE__), 'rink/io_methods'))
-require File.expand_path(File.join(File.dirname(__FILE__), "rink/line_processor/base"))
-require File.expand_path(File.join(File.dirname(__FILE__), "rink/line_processor/pure_ruby"))
-require File.expand_path(File.join(File.dirname(__FILE__), "rink/console"))
-require File.expand_path(File.join(File.dirname(__FILE__), "rink/version"))
 
 module Rink
-  
+  autoload :Delegation, "rink/delegation"
+  autoload :Namespace, "rink/namespace"
+  autoload :Lexer, "rink/lexer"
+  autoload :IOMethods, "rink/io_methods"
+  autoload :LineProcessor, "rink/line_processor"
+  autoload :Console, "rink/console"
+  autoload :Version, "rink/version"
 end
---
+++ lib/rink/line_processor.rb
@@ -0,0 +1,4 @@
+module Rink::LineProcessor
+  autoload :Base, "rink/line_processor/base"
+  autoload :PureRuby, "rink/line_processor/pure_ruby"
+end
